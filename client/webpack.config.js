7d9d5d965f5ed199d7ed20bd8e5281e2504bb84e
Mario Partida <partidam578@gmail.com>
2024-04-22 16:35:47 -0700

    Fixed images not found by setting fingerprints to false in webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
+const { GenerateSW } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -22,6 +23,7 @@ module.exports = () => {
         template: "./index.html",
         title: "JATE",
       }),
+      new GenerateSW(),
       new InjectManifest({
         swSrc: "./src-sw.js",
         swDest: "src-sw.js",
@@ -29,8 +31,10 @@ module.exports = () => {
       new WebpackPwaManifest({
         filename: "manifest.json",
         name: "Yuma-Text-Editor",
+        fingerprints: false,
+        inject: true,
         short_name: "JATE",
-        start_url: "/",
+        start_url: "./",
         background_color: "#ffffff",
         theme_color: "#000000",
         icons: [
