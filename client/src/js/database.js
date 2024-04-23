2cd875b8da691dca3556fd819d7a3b336bd5a175
Mario Partida <partidam578@gmail.com>
2024-04-22 17:25:57 -0700

    Fixed manifest injection by defining the correct path aswell as created logic for database.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,21 +14,22 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  const db = await openDB("jate", 1);
-  const tx = db.transaction("jate", "readwrite");
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readwrite");
   const store = tx.objectStore("jate");
-  await store.add(content);
-  await tx.done;
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log("result", result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  const db = await openDB("jate", 1);
-  const tx = db.transaction("jate", "readonly");
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readonly");
   const store = tx.objectStore("jate");
-  const content = await store.getAll();
-  await tx.done;
-  return content;
+  const request = store.get(1);
+  const result = await request;
+  console.log("result", result);
+  return result?.value;
 };
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,7 +2,6 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
-const { GenerateSW } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -23,10 +22,9 @@ module.exports = () => {
         template: "./index.html",
         title: "JATE",
       }),
-      new GenerateSW(),
       new InjectManifest({
         swSrc: "./src-sw.js",
-        swDest: "src-sw.js",
+        swDest: "./src-sw.js",
       }),
       new WebpackPwaManifest({
         filename: "manifest.json",
@@ -34,9 +32,10 @@ module.exports = () => {
         fingerprints: false,
         inject: true,
         short_name: "JATE",
-        start_url: "./",
         background_color: "#ffffff",
         theme_color: "#000000",
+        start_url: "/",
+        publicPath: "/",
         icons: [
           {
             src: path.resolve("src/images/logo.png"),
